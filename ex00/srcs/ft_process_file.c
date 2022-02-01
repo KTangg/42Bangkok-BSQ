141d01c122a54142a07ae9a3c18c1e59bad6546d
Samboon Poolprasart <spoolpra@z1t5c1.42bangkok.com>
2022-02-01 16:32:01 +0700

    Fix uninitialize

--- ex00/srcs/ft_process_file.c
+++ ex00/srcs/ft_process_file.c
@@ -6,7 +6,7 @@
 /*   By: spoolpra <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 20:31:29 by spoolpra          #+#    #+#             */
-/*   Updated: 2022/02/01 16:17:58 by spoolpra         ###   ########.fr       */
+/*   Updated: 2022/02/01 16:31:15 by spoolpra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,21 @@
 
 int	extract_file(t_info *info, t_index **index_key, int **max)
 {
-	int		n_row;
+	int	i;
+	int	n_row;
 
 	n_row = 0;
+	i = 0;
 	if (!valid_first_line(info, &n_row))
 		return (0);
 	ft_putstr(info->alpha);
 	ft_putstr("\n");
 	*index_key = (t_index *)malloc(sizeof(t_index) * (n_row + 1));
-	index_key[n_row] = 0;
+	while (i <= n_row)
+	{
+		index_key[0][i].begin = NULL;
+		index_key[0][i++].end = NULL;
+	}
 	if (!valid_each_line(info, n_row, *index_key, max))
 	{
 		free(*index_key);
