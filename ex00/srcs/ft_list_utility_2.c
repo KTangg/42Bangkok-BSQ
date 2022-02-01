b87a3c1c05b0fe755b043834f1c595bce06d432d
Samboon Poolprasart <spoolpra@z1t5c1.42bangkok.com>
2022-02-01 16:39:25 +0700

    fix free_index

--- ex00/srcs/ft_list_utility_2.c
+++ ex00/srcs/ft_list_utility_2.c
@@ -6,7 +6,7 @@
 /*   By: spoolpra <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 14:51:12 by spoolpra          #+#    #+#             */
-/*   Updated: 2022/02/01 16:11:53 by spoolpra         ###   ########.fr       */
+/*   Updated: 2022/02/01 16:38:37 by spoolpra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,11 @@ void	free_index(t_index *index_key)
 {
 	t_index	*buffer;
 
-	if (index_key != 0)
+	buffer = index_key;
+	while (index_key->begin != NULL)
 	{
-		buffer = index_key;
-		while (index_key != 0)
-		{
-			ft_free_list(index_key);
-			index_key++;
-		}
-		free(buffer);
+		ft_free_list(index_key);
+		index_key++;
 	}
+	free(buffer);
 }
